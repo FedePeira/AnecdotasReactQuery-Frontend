53bdb31d7d0c7e34a3f83953a609769efcd2e0b8
Federico Peirano <fedepr2345@gmail.com>
2024-03-19 17:27:24 -0300

    d. React Query, useReducer y el contexto - Ejercicios6.21

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -1,10 +1,26 @@
-const AnecdoteForm = () => {
+import { useMutation, useQueryClient  } from '@tanstack/react-query'
+import { createAnecdote } from '../requests'
 
-  const onCreate = (event) => {
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  const newNoteMutation = useMutation({ 
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }
+  })
+
+
+  const onCreate = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
+    if(content.length > 5) {
+      event.target.anecdote.value = ''
+      newNoteMutation.mutate({ content, votes: 0 })
+      console.log('new anecdote')
+    } else {
+      console.error('Error 400: content too short')
+    }
 }
 
   return (
